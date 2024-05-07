5a087ef776a4ddbb70ffb63ee82749ea20d556d1
Gifari Kemal <gifariik@gmail.com>
2024-05-07 10:52:52 +0700

    video 41-42 penambahan many2one avatar dan collaborative field

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -12,11 +12,12 @@ class HospitalAppointment(models.Model): #table (postgres)/ models(odoo)  omod
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date     = fields.Date(string='Booking Date',default=fields.Datetime.now) #format tanggal dapat diubah di settings, languages, en_US, dateformat
     ref              = fields.Char(string='Reference', help="Reference of the patient from the patient record.") #bisa diubah mjd readonly false #field python bisa diberi atr help sebagai penjelasan dari sebuah field
-    prescription     = fields.Html(string='Prescription')
+    prescription     = fields.Html(string='Prescription', options="{'collaborative': True}")
     priority         = fields.Selection(string='Priority', 
                        selection=[('0', 'Normal'), ('1', 'Low'),('2','High'),('3','Very High')]) #penambahan widget priority lewat views
     state            = fields.Selection(string='Status', selection=[('draft', 'Draft'), ('in_consultation', 'In Consultation'),
                        ('done', 'Done'), ('cancel', 'Cancelled')], default="draft", required=True) #state merupakan spesial keyword untuk menambahkan status pada suatu record, required berguna agar tidak ada record dengan state = 0
+    doctor_id = fields.Many2one(comodel_name='res.users', string='Doctor') #Field many2one dengan model res.users(base model dari odoo(rumit))
     
     
     #api onchange berubah saat terjadi saving record berbeda dengan depends
