70ca608da0cd23780856142287055d5bae38f413
Gifari Kemal <gifariik@gmail.com>
2024-05-08 21:56:21 +0700

    penambahan integer field color dan menambahkannya widget color_picker

--- om_hospital/models/patient_tag.py
+++ om_hospital/models/patient_tag.py
@@ -8,5 +8,7 @@ class PatientTag(models.Model):
 
     name = fields.Char(string='Name', required=True)
     active = fields.Boolean(string='Active', default=True)
+    color = fields.Integer(string='Color')
+    
     
     
