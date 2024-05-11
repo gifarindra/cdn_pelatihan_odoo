0100771bb9c1175eeca7ac5516cf403320005bd1
Gifari Kemal <gifariik@gmail.com>
2024-05-12 00:31:38 +0700

    video 69 model inheritance, view inheritance, dan penambahan field saat inheritance view

--- om_odoo_inheritance/models/sale_order.py
+++ om_odoo_inheritance/models/sale_order.py
@@ -5,5 +5,5 @@ from odoo import models, fields, api
 class SaleOrder(models.Model): #omodi snippet untuk model inheritance
     _inherit = 'sale.order'    #tanpa _name hanya _inherit dengan nama model yang diinherit
 
-    def test(self):
-        return
+    confirmed_user_id = fields.Many2one(comodel_name='res.users', string='Confirmed Users') #penambahan field baru pada model sale.order dari model res.users, perubahandpt dilihat di techn -> models -> fields -> misc
+    
