5edd4afc64a049f635e3a3b65b894d55fc864b63
Gifari Kemal <gifariik@gmail.com>
2024-05-12 01:00:55 +0700

    video 70 function inheritance dengan super

--- om_odoo_inheritance/models/sale_order.py
+++ om_odoo_inheritance/models/sale_order.py
@@ -7,3 +7,6 @@ class SaleOrder(models.Model): #omodi snippet untuk model inheritance
 
     confirmed_user_id = fields.Many2one(comodel_name='res.users', string='Confirmed Users') #penambahan field baru pada model sale.order dari model res.users, perubahandpt dilihat di techn -> models -> fields -> misc
     
+    def action_confirm(self):
+        print('yippie!') #statement akan dieksekusi karena function telah diinherit menggunakan super, posisi super menentukan function mana yang akan dieksekusi tlbh dahulu
+        super(SaleOrder,self).action_confirm() #untuk mewariskan function, menggunakan super, super(ClassNamePewaris, self(tergantungargumen function)).nama_function()
