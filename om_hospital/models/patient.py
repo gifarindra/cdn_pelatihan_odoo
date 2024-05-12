eef77997cd7a903671f4dea5636b46a7b60c2092
Gifari Kemal <gifariik@gmail.com>
2024-05-12 12:42:08 +0700

    video 73 write method(editing process) inheritance dan penetapan sequence pada ref yang tidak bervalue dan tidak diisi pada proses write(editing)

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -17,18 +17,24 @@ class HospitalPatient(models.Model): #table (postgres)/ models(odoo)  omod
     image          = fields.Image(string='Image')
     tag_ids        = fields.Many2many(comodel_name='patient.tag', string='Patient Tags') #penjelasan lengkap mengenai mekanisme many2many bisa dilihat di odoomates 15 tutorial vid 57
     
-    @api.model #perlu diberikan saat menginherit method (hanya create method saja??) dari model
-    def create(self, vals): #oocreate snippet, untuk menginherit create method dari Models, ooverride snippet untuk override create method
+    @api.model #perlu diberikan dekorator saat menginherit method (hanya create method saja??) dari model
+    def create(self, vals): #ocreate snippet, untuk menginherit create method dari Models, ooverride snippet untuk override create method
         vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient') #Penetapan value dari field juga dapat dilakukan secara implisit lewat inherit create method dengan mengambil field 'ref' dan menetapkannya thdp suatu nilai (dalam hal ini string OMTEST), nilai OMTEST akan menimpa nilai dari ref jk dibuat dari form view,
         return super(HospitalPatient, self). create(vals) #function/method inheritance dari create() menggunakan super funct, arg vals berisi field2 yang telah dibuat pada model, return perlu diberikan
         #tipe struktur data dari variabel vals adalah dictionary dengan key = field dan value = value_field
     #penetapan value inherit create method jg berguna untuk sequencing dengan sintaks orm method, dimana self.env akan memanggil model ir.sequence dan memanggil fungsi next_by_code(fungsi berada di ir.sequence) dengan variable code_name_sequence dimana value yang dipanggil adalah field code
     
+    def write(self, vals): #owrite snippet, tanpa dekorator untuk write method, write method dipanggil saat ada editing(bukan creating) record
+        # jk print vals, yg akan diprint hanya value yang berubah saja tdk semua field diprint oleh vals
+        if not self.ref and not vals.get('ref'): #pengecekan jika data ref tidak mempunyai value dan tidak diisi saat proses write(editing)
+            vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient') #maka sequence selanjutnya akan diberikan ke record yang telah diedit
+        return super(HospitalPatient, self).write(vals)
+    
     
     @api.depends('date_of_birth') #sebuah dekorator api untuk membuat compute function berfungsi segera setelah ada perubahan thd dob, bukan saat saving
     def _compute_age(self):
         for rec in self: # iterasi dilakukan karena field yang akan dicompute dimiliki oleh banyak record agar tdk terjadi singleton error
-            today = date.today() # memanggil fungsi today() dari modul py date dengan variable today
+            today       = date.today() # memanggil fungsi today() dari modul py date dengan variable today
             if rec.date_of_birth: #mengambil tahun dari field dob jika field dob ada value
                 rec.age = today.year - rec.date_of_birth.year # membuat field age menjadi tahun skrg - tahun dob
             else:
