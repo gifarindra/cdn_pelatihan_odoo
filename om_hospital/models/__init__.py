281f6d0eaa1625569f67d5c9c00f73b86e4038e2
Gifari Kemal <gifariik@gmail.com>
2024-05-15 22:11:48 +0700

    aa my github error!!

--- om_hospital/models/__init__.py
+++ om_hospital/models/__init__.py
@@ -5,4 +5,4 @@ from . import patient
 from . import appointment
 from . import patient_tag
 from . import odoo_playground
-from . import res_config_settings #impor model res.config.settings
+from . import res_config_settings #import model res.config.settings
